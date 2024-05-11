bba0b5a2e3f595d37abb665fdf7738935f2e2e28
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-05-11 15:26:06 -0700

    Added a button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,38 @@
 'use client';
 
+import { useCallback } from 'react';
 import { Tldraw } from 'tldraw';
 import './index.css';
+import { FloatButton } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
 
 export default function Home() {
     return (
         <div style={{ position: 'fixed', inset: 0 }}>
-            <Tldraw />
+            <Tldraw
+                persistenceKey="things-on-the-canvas-example"
+                components={{
+                    InFrontOfTheCanvas: useCallback(() => {
+                        return (
+                            <FloatButton
+                                type="primary"
+                                icon={
+                                    <AudioOutlined
+                                        style={{ fontSize: '20px' }}
+                                    />
+                                }
+                                style={{
+                                    position: 'absolute',
+                                    width: 80,
+                                    height: 80,
+                                    bottom: 100,
+                                    left: 10,
+                                }}
+                            />
+                        );
+                    }, []),
+                }}
+            />
         </div>
     );
 }
